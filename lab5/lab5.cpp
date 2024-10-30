533a96114de5f5fc1324ef2cde91acba188b5d1c
Andrew-V-o <andrew.vodonosov@gmail.com>
2024-10-30 14:50:57 +0300

    laba 5

---
+++ lab5/lab5.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+int i;
+string str;
+
+bool ravny(string str)
+{
+    for (int i = 0; i < (str.length() - 1); i++)
+    {
+
+        if (str[i] != str[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool palindromes(string str)
+{
+
+    for (int i = 0; i < ((str.length() - 1) / 2); i++)
+    {
+
+        if (str[i] != str[(str.length() - 1) - i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    SetConsoleOutputCP(65001);
+    ofstream fout;
+
+    fout.open("ghf.txt");
+    int n;
+    cout << "введите количество строк: ";
+    cin >> n;
+    cin.ignore();
+
+    if (!fout.is_open())
+    {
+        cout << "Ошибка открытия файла" << endl;
+    }
+
+    else
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            string str;
+            cout << "введите строку: ";
+            getline(cin, str);
+            fout << str << endl;
+        }
+    }
+    fout.close();
+    ifstream fin("ghf.txt");
+    ofstream palindromesFile("palindromes.txt");
+    ofstream ravnyFile("ravny.txt");
+
+    while (getline(fin, str))
+    {
+        if (palindromes(str))
+        {
+            palindromesFile << str << endl;
+        }
+        if (ravny(str))
+        {
+            ravnyFile << str << endl;
+        }
+    }
+   palindromesFile.close();
+   ravnyFile.close();
+
+    /*
+        ifstream fin;
+
+        fin.open("ghf.txt");
+
+        if (!fin.is_open())
+        {
+            cout << "Ошибка открытия файла" << endl;
+        }
+
+        else
+        {
+            string str2;
+            while (!fin.eof())
+            {
+                getline(fin, str2);
+                cout << str2;
+            }
+        }
+        fin.close();
+        */
+}
